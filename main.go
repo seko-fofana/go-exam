dd97e909582d0bc777a1821178282b7a31a022c0
seko-fofana <77271208+seko-fofana@users.noreply.github.com>
2021-01-15 16:57:54 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 }
 
 type Task struct {
-	description string
-	done        bool
+	Description string
+	Done        bool
+}
+
+var task []Task
+
+func list(rw http.ResponseWriter, _ *http.Request) {
+	fmt.Println("list")
+}
+func done(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("done")
+}
+func add(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("add")
+}
+func main() {
+
+	http.HandleFunc("/", list)
+	http.HandleFunc("/done", done)
+	http.HandleFunc("/add", add)
+
+	http.ListenAndServe(":8080", nil)
 }
